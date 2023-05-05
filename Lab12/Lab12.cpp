330ecb2a9feaf6edf1ddcf10a003fef5da858809
Кирил <@DESKTOP-0C9GOP3>
2023-05-05 15:54:08 +0300

    Добавьте файлы проекта.

---
+++ Lab12/Lab12.cpp
@@ -0,0 +1,56 @@
+﻿#include<iostream>
+#include <fstream>
+#include <string>
+#include<Windows.h>
+
+using namespace std;
+
+int main() {
+	
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	string filename = "числа.txt";
+	
+	ifstream infile(filename);
+	
+	int z;
+	cout << "Ведіть Число  z = ";
+	
+	cin >> z;
+	if (infile) { //перевіряємо, що файл був успішно відкритий
+		
+		
+
+		int num, sum = 0;
+		
+		while (infile >> num) {
+
+			/*if (num > z) {
+			                   //Обратный вариант завдання 
+				sum += num;
+
+			}*/
+
+			if (num < z) {
+
+				sum += num;
+			
+			}
+
+			
+		}
+
+		cout << "Сума чисел, меньше   " << z << ":" << sum << endl;
+	    //cout << "Сума чисел, більших " << z << ":" << sum << endl;
+
+			infile.close();
+		
+	}
+	else {
+		
+		cout << "Помилка відкритя файлу " << filename << endl;
+
+	}
+	return 0;
+}
